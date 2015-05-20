90ba098a430c818790cc1d2c85a88e8d90a7b55b
Risto Haukioja <risto.haukioja@gmail.com>
2015-05-19 22:26:37 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dhmanager
 Python Library for Managing DreamHost DNS records
+
+Manage your DNS Records at DreamHost.
+
+1) Read first the (rather limited) API documentation from:
+    http://wiki.dreamhost.com/API
+2) Then register an API key via:
+    https://panel.dreamhost.com/?tree=home.api
+3) Store the api key in this folder in file called KEYFILE
+4) run: 
+```
+python dhmanager.py
+```
+
+
--- dhmanager.py
+++ dhmanager.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*- -
 """dhmanager.py
 
-Manage your DNS Records at DreamHost.
-
-1) Read first the (rather limited) API documentation from:
-    http://wiki.dreamhost.com/API
-2) Then register an API key via:
-    https://panel.dreamhost.com/?tree=home.api
-3) Store the api key in this folder in file called KEYFILE
-4) run: python dhmanager.py
-
+Library for managing DreamHost DNS Records. See README.md for more details.
 Created by: risto.haukioja@gmail.com
 """
 import uuid
